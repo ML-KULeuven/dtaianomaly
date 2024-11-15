4ed0eb240092c513ad4225d00108a2db834803fa
LouisCarpentier42 <louis.carpentier@kuleuven.be>
2024-11-15 09:50:56 +0100

    Update testing of PyOD detectors

--- tests/anomaly_detection/test_HistogramBasedOutlierScore.py
+++ tests/anomaly_detection/test_HistogramBasedOutlierScore.py
@@ -1,46 +1,8 @@
 
-import pytest
 from dtaianomaly.anomaly_detection import HistogramBasedOutlierScore
 
 
-class TestIsolationForest:
-
-    def test_initialize(self):
-        detector = HistogramBasedOutlierScore(15, n_bins=42)
-        assert detector.window_size == 15
-        assert detector.stride == 1
-        assert detector.kwargs['n_bins'] == 42
-
-    def test_initialize_too_small_window_size(self):
-        with pytest.raises(ValueError):
-            HistogramBasedOutlierScore(window_size=0)
-        HistogramBasedOutlierScore(1)  # Doesn't raise an error with float
-        HistogramBasedOutlierScore(15)  # Doesn't raise an error with int
-
-    def test_initialize_float_window_size(self):
-        with pytest.raises(ValueError):
-            HistogramBasedOutlierScore(window_size=5.5)
-
-    def test_initialize_valid_string_window_size(self):
-        HistogramBasedOutlierScore(window_size='fft')
-
-    def test_initialize_string_window_size(self):
-        with pytest.raises(ValueError):
-            HistogramBasedOutlierScore(window_size='15')
-
-    def test_initialize_too_small_stride(self):
-        with pytest.raises(ValueError):
-            HistogramBasedOutlierScore(window_size=15, stride=0)
-        HistogramBasedOutlierScore(window_size=15, stride=1)  # Doesn't raise an error with float
-        HistogramBasedOutlierScore(window_size=15, stride=5)  # Doesn't raise an error with int
-
-    def test_initialize_float_stride(self):
-        with pytest.raises(TypeError):
-            HistogramBasedOutlierScore(window_size=10, stride=2.5)
-
-    def test_initialize_string_stride(self):
-        with pytest.raises(TypeError):
-            HistogramBasedOutlierScore(window_size=10, stride='1')
+class TestHistogramBasedOutlierScore:
 
     def test_str(self):
         assert str(HistogramBasedOutlierScore(5)) == "HistogramBasedOutlierScore(window_size=5)"
