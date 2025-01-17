609994b6a5bd19c544be4489613d8210934b851a
LouisCarpentier42 <louis.carpentier@kuleuven.be>
2025-01-17 15:20:34 +0100

    Implemented ExCeed

--- dtaianomaly/anomaly_detection/BaseDetector.py
+++ dtaianomaly/anomaly_detection/BaseDetector.py
@@ -3,9 +3,11 @@ import os.path
 import pickle
 import enum
 import numpy as np
+import scipy
 from pathlib import Path
 from typing import Optional, Union
 
+from dtaianomaly.thresholding.thresholding import ContaminationRate
 from dtaianomaly import utils
 from dtaianomaly.PrettyPrintable import PrettyPrintable
 
@@ -124,6 +126,64 @@ class BaseDetector(PrettyPrintable):
 
         else:
             return (raw_scores - min_score) / (max_score - min_score)
+
+    def predict_confidence(self, X: np.ndarray, X_train: np.ndarray = None, contamination: float = 0.05):
+        """
+        Predict the confidence of the anomaly scores on the test given test data.
+
+        This method implements ExCeeD [perini2020quantifying]_ (Example-wise Confidence
+        of anomaly Detectors) to estimate the confidence. ExCeed transforms the predicted
+        decision scores to probability estimates using a Bayesian approach, which enables
+        to assign a confidence score to each prediction which captures the uncertainty
+        of the anomaly detector in that prediction.
+
+        Parameters
+        ----------
+        X: array-like of shape (n_samples, n_attributes)
+            The test time series for which the confidence of anomaly scores
+            should be predicted.
+        X_train: array-like of shape (n_samples_train, n_attributes), default=None
+            The training time series, which can be used as reference. If
+            ``X_train=None``, the test set is used as reference set.
+        contamination: float, default=0.05
+            The (estimated) contamination rate for the data, i.e., the expected
+            percentage of anomalies.
+
+        Returns
+        -------
+        confidence: array-like of shape (n_samples)
+            The confidence of this anomaly detector in each prediction in the
+            given test time series.
+
+        References
+        ----------
+        .. [perini2020quantifying] Perini, L., Vercruyssen, V., Davis, J. Quantifying
+           the Confidence of Anomaly Detectors in Their Example-Wise Predictions. In:
+           Machine Learning and Knowledge Discovery in Databases. ECML PKDD 2020.
+           Springer, Cham, doi: `10.1007/978-3-030-67664-3_14 <https://doi.org/10.1007/978-3-030-67664-3_14>`_.
+        """
+
+        # Compute the decision scores
+        decision_scores = self.decision_function(X)
+        decision_scores_train = self.decision_function(X_train) if X_train is not None else decision_scores
+
+        # Convert the decision scores to binary predictions
+        prediction = ContaminationRate(contamination_rate=contamination).threshold(decision_scores)
+
+        # Apply the ExCeed method (https://github.com/Lorenzo-Perini/Confidence_AD/blob/master/ExCeeD.py)
+        n = decision_scores.shape[0]
+
+        count_instances = np.vectorize(lambda x: np.count_nonzero(decision_scores_train <= x))
+        n_instances = count_instances(decision_scores)
+
+        prob_func = np.vectorize(lambda x: (1 + x) / (2 + n))
+        posterior_prob = prob_func(n_instances)  # Outlier probability according to ExCeeD
+
+        conf_func = np.vectorize(lambda p: 1 - scipy.stats.binom.cdf(int(n * contamination), n, p))
+        exWise_conf = conf_func(posterior_prob)
+        np.place(exWise_conf, prediction == 0, 1 - exWise_conf[prediction == 0])  # if the example is classified as normal, use 1 - confidence.
+
+        return exWise_conf
 
     def save(self, path: Union[str, Path]) -> None:
         """
